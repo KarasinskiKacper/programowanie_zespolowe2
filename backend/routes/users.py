819e0ae8cf60454b89c237c938da13e98380d2e4
Paweł Dyczek <paweldyczek02@gmail.com>
2025-12-03 07:56:15 +0100

    Add get online users to API

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from db_objects import Users, db, Users_room, Chat_history, Rooms
+from user_activity import get_online_users as get_online_users_from_app_state
 
 bp = Blueprint('users', __name__, url_prefix='/api')
 
@@ -7,6 +8,16 @@ bp = Blueprint('users', __name__, url_prefix='/api')
 def get_users():
     users = Users.query.all()
     return jsonify([user.to_dict() for user in users])
+
+@bp.route("users/online", methods=['GET'])
+def get_online_users():
+    if request.args.get("user_name"):
+        user_name = request.args.get("user_name")
+        online_users = get_online_users_from_app_state(user_name)
+    else:
+        return jsonify({"error": "Missing user_name parameter"}), 400
+
+    return jsonify(list(online_users))
 
 @bp.route('/user', methods=['GET'])
 def get_user():
